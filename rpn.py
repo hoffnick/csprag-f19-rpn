c1bce029448015e23ac964a18cef33dad5c7b998
Nick Hoffman <hoffnick@umich.edu>
2019-11-01 14:10:05 -0400

    Refactor

--- rpn.py
+++ rpn.py
@@ -1,23 +1,41 @@
 #!/usr/bin/env python3
+
+import operator
+
+operators = {
+	'+': operator.add,
+	'-': operator.sub,
+}
+
 def calculate(arg):
 	stack = list()
 	for token in arg.split():
-		if token == '+':
-			arg1 = stack.pop()
+		try:
+			value = int(token)
+			stack.append(value)
+		except ValueError:
+			function = operators[token]
 			arg2 = stack.pop()
-			result = arg1 + arg2
-			stack.append(result)
-		elif token == '-':
 			arg1 = stack.pop()
-			arg2 = stack.pop()
-			result = arg2 - arg1
+			result = function(arg1, arg2)
 			stack.append(result)
-		else:
-			stack.append(int(token))
-		#print(stack)
-	if len(stack) != 1:
-		raise TypeError("malfored input")
-	return(stack.pop())
+
+	# 	if token == '+':
+	# 		arg1 = stack.pop()
+	# 		arg2 = stack.pop()
+	# 		result = arg1 + arg2
+	# 		stack.append(result)
+	# 	elif token == '-':
+	# 		arg1 = stack.pop()
+	# 		arg2 = stack.pop()
+	# 		result = arg2 - arg1
+	# 		stack.append(result)
+	# 	else:
+	# 		stack.append(int(token))
+	# 	#print(stack)
+	# if len(stack) != 1:
+	# 	raise TypeError("malfored input")
+	# return(stack.pop())
 
 def main():
 	while True:
